623477b41332c06fa20918b2e42786b9edc08c6d
FIS-21 <lgiereth@gamil.com>
2020-03-04 10:12:12 +0100

    New print out (Was geht?)

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -4,7 +4,9 @@ class HalloKurs{
 		System.out.println("Hallo an alle!");
 		System.out.println("Hallo Dozent!");
 		System.out.println("Python ist besser!\nKein Scheiß!");
+
 		System.out.println("Nicht dein Ernst ?!?!?!");
 		System.out.println("DOCH!");
+		System.out.println("Was geht Leute?");
 	}
 }
