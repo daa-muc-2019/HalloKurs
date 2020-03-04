27185c9c14fa05f7a60e063f6870c52633572ac7
Bünyamin Kurt <58681429+moostey@users.noreply.github.com>
2020-03-04 11:17:40 +0100

    alles weg

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -1,26 +1,6 @@
 class HalloKurs{
 
 	public static void main(String[] args){
-		System.out.println("Wer das liest ist cool.");
-		System.out.println("Hallo Dozent!");
-		System.out.println("Python ist besser!\nKein Scheiß!");
-		System.out.println("Sei ruhig Freibier.");
 
-		System.out.println("Sei ruhig Freibier.");
-		System.out.println("Nicht dein Ernst ?!?!?!");
-		System.out.println("DOCH!");
-		System.out.println("Was geht Leute seit ihr mit mir down?");
-		System.out.println("Sympathisch, man kann sich Unterhalten!");
-        System.out.println("Corona an alle, Freibert gibt aus");
-        System.out.println("Bergfest");
-        System.out.println("Ohne Meldung.");
-		System.out.println("Why this don't running ?");
-		System.out.println("kek");
-		System.out.println("LOL");
-		System.out.println("Freibier für ALLE");
-		System.out.println("Why are u Runnin!??!");
-		System.out.println("Test");
-		System.out.println("Test Test");
-		System.out.println("<UwU>,,    (๑òᆺó๑)    ");
 	}
 }
