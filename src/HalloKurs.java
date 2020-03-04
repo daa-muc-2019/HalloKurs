a466657b7be9d407b73f52401c9b545aade0d184
Bünyamin Kurt <58681429+moostey@users.noreply.github.com>
2020-03-04 11:22:49 +0100

    123

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -2,7 +2,9 @@ class HalloKurs{
 
 	public static void main(String[] args){
 
-		system.out.println("Das ist mir herrrzlich egal, ist es mir!");
+		System.out.println("Das ist mir herrrzlich egal, ist es mir!");
+		System.out.println("Hallo meine Lieben!");
+		System.out.println("Das MÜSST ihr wissen!);
 
 	}
 }
