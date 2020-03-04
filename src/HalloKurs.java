a20a133077383ffe9b3caf07f5c9c4ec6e771dc5
Thomas <gregor-slade@hotmail.de>
2020-03-04 10:13:38 +0100

    print freibier

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -4,10 +4,10 @@ class HalloKurs{
 		System.out.println("Hallo an alle!");
 		System.out.println("Hallo Dozent!");
 		System.out.println("Python ist besser!\nKein Scheiß!");
-        System.out.println("Corona an alle, Freibert gibt aus");
 		System.out.println("Nicht dein Ernst ?!?!?!");
 		System.out.println("DOCH!");
 		System.out.println("Sympathisch, man kann sich Unterhalten!");
+        System.out.println("Corona an alle, Freibert gibt aus");
 
 
 	}
