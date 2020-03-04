811ef4ff8ab8c8c1e45c8b654152ccf70952320b
NoRisk-NoFun <58681974+NoRisk-NoFun@users.noreply.github.com>
2020-03-04 11:24:38 +0100

    New print

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -4,7 +4,8 @@ class HalloKurs{
 
 		System.out.println("Das ist mir herrrzlich egal, ist es mir!");
 		System.out.println("Hallo meine Lieben!");
-		System.out.println("Das MÜSST ihr wissen!);
+		System.out.println("Das MÜSST ihr wissen!");
+		System.out.println("Das müsst Ihr euch Merken, das ist Prüfungs-Relevant!");
 
 	}
 }
