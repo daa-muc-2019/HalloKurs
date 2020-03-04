d8b5d1b6d2a44deeb62051063e087a95a1282725
TimFre <57749877+TimFre@users.noreply.github.com>
2020-03-04 10:04:50 +0100

    New Print

--- src/HalloKurs.java
+++ src/HalloKurs.java
@@ -5,6 +5,6 @@ class HalloKurs{
 		System.out.println("Hallo Dozent!");
 		System.out.println("Python ist besser!\nKein Scheiß!");
 		System.out.println("Nicht dein Ernst ?!?!?!");
-
+		System.out.println("DOCH");
 	}
 }
